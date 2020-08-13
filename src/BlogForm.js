406140580bcfc05970f14a4fa747964ce5d2de1f
Dru Serkes <DruSerkes@gmail.com>
2020-08-13 14:03:09 -0400

    add buttons

--- src/BlogForm.js
+++ src/BlogForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Formik, Form } from 'formik';
 import validationSchema from './blogSchema';
 import FormikInput from './FormikInput';
@@ -18,6 +19,12 @@ const BlogForm = ({ title = '', description = '', body = '' }) => {
 				<FormikInput name="title" type="text" />
 				<FormikInput name="description" type="text" />
 				<FormikInput name="body" type="textarea" />
+				<button type="submit" className="Save-Button">
+					Save
+				</button>
+				<button className="Cancel">
+					<Link to="/">Cancel</Link>
+				</button>
 			</Form>
 		</Formik>
 	);
