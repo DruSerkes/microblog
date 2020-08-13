11a346bc9ea997db34cbf555b7c52d89f049ff32
Dru Serkes <DruSerkes@gmail.com>
2020-08-13 14:17:01 -0400

    install uuid. write saveBlog function

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 
 const Routes = () => {
+	const [ blogs, setBlogs ] = useState(null);
+
+	const saveBlog = ({ title, description, body }) => {
+		setBlogs((blogs) => [ ...blogs, { title, description, body, id: uuid() } ]);
+	};
+
 	return (
 		<Switch>
 			<Route exact path="/add">
