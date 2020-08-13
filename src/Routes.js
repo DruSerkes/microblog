4aa7fd11756ff6cc52f9f3c9d35049fa85e0c160
Dru Serkes <DruSerkes@gmail.com>
2020-08-13 14:05:17 -0400

    add post details route

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 const Routes = () => {
 	return (
 		<Switch>
-			<Route exact path="/">
-				<h2>Blog</h2>
-			</Route>
 			<Route exact path="/add">
 				<h2>Add</h2>
+			</Route>
+			<Route exact path="/:id">
+				<h2>Post details</h2>
+			</Route>
+			<Route exact path="/">
+				<h2>Blog</h2>
 			</Route>
 			<Redirect to="/" />
 		</Switch>
