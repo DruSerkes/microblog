6e3c9840e390392837f4b7e8302b23417e1f763f
Dru Serkes <DruSerkes@gmail.com>
2020-08-14 13:13:06 -0400

    add title actions

--- src/reducers/actionTypes.js
+++ src/reducers/actionTypes.js
@@ -1,5 +1,11 @@
-export const ADD_POST = 'ADD';
-export const REMOVE_POST = 'REMOVE';
-export const EDIT_POST = 'EDIT';
+// POSTS
+export const ADD_POST = 'ADD_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+// COMMENTS
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+// TITLES
+export const ADD_TITLE = 'ADD_TITLE';
+export const EDIT_TITLE = 'EDIT_TITLE';
+export const REMOVE_TITLE = 'REMOVE_TITLE';
