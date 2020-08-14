9aa7b27298d72b803c11c19ee32274fc04ea795f
Dru Serkes <DruSerkes@gmail.com>
2020-08-14 13:10:59 -0400

    create store add Provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+// next step you will also import compose, combineReducers from redux
+import { Provider } from 'react-redux';
+import postReducer from './reducers/postReducer';
+
+const store = createStore(postReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX__DEVTOOLS__EXTENSION__());
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
