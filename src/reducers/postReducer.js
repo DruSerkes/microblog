835f25054436078aa5587f6874b969e02900771b
Dru Serkes <DruSerkes@gmail.com>
2020-08-14 13:22:30 -0400

    create titleReducer, update returns in postReducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -4,22 +4,22 @@ const INITIAL_STATE = {};
 const postReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case ADD_POST:
-			return { ...state, posts: { ...posts, [action.id]: action.post } };
+			return { ...state, posts: { ...state.posts, [action.id]: action.post } };
 		case REMOVE_POST:
 			const updatedPosts = { ...state.posts };
 			delete updatedPosts[action.id];
 			return { ...state, posts: updatedPosts };
 		case EDIT_POST:
-			return { ...state, posts: { ...posts, [action.id]: action.post } };
+			return { ...state, posts: { ...state.posts, [action.id]: action.post } };
 		case ADD_COMMENT:
 			const post = state.posts[action.id];
 			post.comments = [ ...post.comments, action.comment ];
-			return { ...state, posts: { ...posts, [action.id]: post } };
+			return { ...state, posts: { ...state.posts, [action.id]: post } };
 		case REMOVE_COMMENT:
 			const post = state.posts[action.id];
 			const newComments = post.comments.filter((comment) => comment.id !== action.commentId);
 			post.comments = newComments;
-			return { ...state, posts: { ...posts, [action.id]: post } };
+			return { ...state, posts: { ...state.posts, [action.id]: post } };
 		default:
 			return state;
 	}
--- src/reducers/titleReducer.js
+++ src/reducers/titleReducer.js
@@ -1,5 +1,17 @@
 import { ADD_TITLE, EDIT_TITLE, REMOVE_TITLE } from './actionTypes';
-const INITIAL_STATE = {}
+const INITIAL_STATE = {};
+
 const titleReducer = (state = INITIAL_STATE, action) => {
-    switch
-}
+	switch (action.type) {
+		case ADD_TITLE:
+			return { ...state, titles: { ...state.titles, [action.id]: action.title } };
+		case REMOVE_TITLE:
+			const updatedTitles = { ...state.titles };
+			delete updatedTitles[action.id];
+			return { ...state, titles: updatedTitles };
+		case EDIT_TITLE:
+			return { ...state, titles: { ...state.titles, [action.id]: action.post } };
+	}
+};
+
+export default titleReducer;
