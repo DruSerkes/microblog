a5520519c8e6eee91d3c940c1d5fdd51bd1ddc3b
Dru Serkes <DruSerkes@gmail.com>
2020-08-15 00:34:08 -0400

    remove unused variables

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import BlogList from './BlogList';
 
-const Homepage = ({ blogs }) => {
+const Homepage = () => {
 	return (
 		<div className="Homepage">
 			<p>
 				Welcome to <b>Microblog,</b> our innovative site for communicating on the information super highway!
 			</p>
-			<BlogList blogs={blogs} />
+			<BlogList />
 		</div>
 	);
 };
