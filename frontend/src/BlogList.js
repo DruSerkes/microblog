13fc3e67aa155b41d1a74bc0f69fc0e94ae6e4f3
Dru Serkes <DruSerkes@gmail.com>
2020-08-15 00:46:33 -0400

    BlogList gets titles from API and stores in Redux

--- frontend/src/BlogList.js
+++ frontend/src/BlogList.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BlogCard from './BlogCard';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTitles } from './reducers/actions';
 
 const BlogList = () => {
+	const dispatch = useDispatch();
 	const titles = useSelector((state) => state.titles);
-	const blogIds = Object.keys(titles);
+	const titleKeys = Object.keys(titles);
+
+	useEffect(
+		() => {
+			dispatch(getTitles());
+		},
+		[ dispatch ]
+	);
+
 	return (
 		<div className="BlogList">
-			{blogIds.map((id) => {
-				let { title, description } = titles[id];
-				return <BlogCard title={title} description={description} id={id} key={id} />;
+			{titleKeys.map((key) => {
+				let { title, description, id, votes } = titles[key];
+				return <BlogCard title={title} description={description} id={id} key={id} votes={votes} />;
 			})}
 		</div>
 	);
